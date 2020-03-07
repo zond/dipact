9e1b0b08f16e66e734a2c20ea4a37142ab8c28b5
tttppp <tttppp@users.noreply.github.com>
2020-03-07 08:43:22 +0000

    Maybe a list item needs list item text in it?

--- templates/js/notifications.js
+++ templates/js/notifications.js
@@ -7,13 +7,13 @@ export default class Notifications extends React.Component {
 			<MaterialUI.div>
 				<MaterialUI.List>
 					<MaterialUI.ListItem div>
-						NEWS: Diplicity has a new React-based UI.
+						<MaterialUI.ListItemText primary="NEWS: Diplicity has a new React-based UI." />
 					</MaterialUI.ListItem>
 					<MaterialUI.ListItem div>
-						MESSAGE: [Fra-Eng] Please stop bouncing in the English Channel.
+						<MaterialUI.ListItemText primary="MESSAGE: [Fra-Eng] Please stop bouncing in the English Channel." />
 					</MaterialUI.ListItem>
 					<MaterialUI.ListItem div>
-						INFO: No idea how this will be populated yet.
+						<MaterialUI.ListItemText primary="INFO: No idea how this will be populated yet." />
 					</MaterialUI.ListItem>
 				</MaterialUI.List>
 			</MaterialUI.div>
