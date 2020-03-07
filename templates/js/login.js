d89ee604d2ee80e39886e8427244c6de8d864599
tttppp <tttppp@users.noreply.github.com>
2020-03-07 08:16:52 +0000

    Indentation of JSX.

--- templates/js/login.js
+++ templates/js/login.js
@@ -4,13 +4,15 @@ export default class Login extends React.Component {
 		if (this.props.loginURL) {
 			return (
 				<MaterialUI.Container style={{textAlign: 'center'}} maxWidth="sm">
-				<MaterialUI.Button variant="contained" startIcon={<i className="material-icons">&#xE898;</i>}><a href={this.props.loginURL}>Login</a></MaterialUI.Button>
+					<MaterialUI.Button variant="contained" startIcon={<i className="material-icons">&#xE898;</i>}>
+						<a href={this.props.loginURL}>Login</a>
+					</MaterialUI.Button>
 				</MaterialUI.Container>
 			);
 		} else {
 			return (
 				<MaterialUI.Container style={{textAlign: 'center'}} maxWidth="sm">
-				<MaterialUI.CircularProgress />
+					<MaterialUI.CircularProgress />
 				</MaterialUI.Container>
 			);
 		}
