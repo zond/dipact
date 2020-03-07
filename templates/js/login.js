27095673ea6e5cb2d3c939fb9bb1522c43fcec32
Martin Bruse <zondolfin@gmail.com>
2020-03-07 03:10:52 +0100

    Center align. Added viewport.

--- templates/js/login.js
+++ templates/js/login.js
@@ -3,13 +3,13 @@ export default class Login extends React.Component {
 	render() {
 		if (this.props.loginURL) {
 			return (
-				<MaterialUI.Container maxWidth="sm">
+				<MaterialUI.Container style={{textAlign: 'center'}} maxWidth="sm">
 				<MaterialUI.Button variant="contained" startIcon={<i className="material-icons">&#xE898;</i>}><a href={this.props.loginURL}>Login</a></MaterialUI.Button>
 				</MaterialUI.Container>
 			);
 		} else {
 			return (
-				<MaterialUI.Container maxWidth="sm">
+				<MaterialUI.Container style={{textAlign: 'center'}} maxWidth="sm">
 				<MaterialUI.CircularProgress />
 				</MaterialUI.Container>
 			);
