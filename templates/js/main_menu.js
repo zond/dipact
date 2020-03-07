4dabc03e02456c291a69889405bdd405a26625cb
Martin Bruse <zondolfin@gmail.com>
2020-03-07 14:33:13 +0100

    Made the menu drawer close on clicks outside the menu.

--- templates/js/main_menu.js
+++ templates/js/main_menu.js
@@ -4,43 +4,49 @@ export default class MainMenu extends React.Component {
 		this.state = {
 			drawerOpen: false
 		};
-		this.toggleDrawer = this.toggleDrawer.bind(this);
+		this.openDrawer = this.openDrawer.bind(this);
+		this.closeDrawer = this.closeDrawer.bind(this);
 	}
-	toggleDrawer() {
-		this.setState({drawerOpen: !this.state.drawerOpen});
+	openDrawer() {
+		this.setState({drawerOpen: true});
+	}
+	closeDrawer() {
+		this.setState({drawerOpen: false});
 	}
 	render() {
 		return (
 			<div>
-				<MaterialUI.IconButton onClick={this.toggleDrawer}>
+				<MaterialUI.IconButton onClick={this.openDrawer}>
 					<i className="material-icons">&#xE5D2;</i>
 				</MaterialUI.IconButton>
 				<MaterialUI.Drawer open={this.state.drawerOpen}>
-					<div onClick={this.toggleDrawer}>
-						<MaterialUI.List component="nav">
-							<MaterialUI.ListItem button>
-								<MaterialUI.ListItemText primary="Notifications" />
-							</MaterialUI.ListItem>
-							<MaterialUI.ListItem button>
-								<MaterialUI.ListItemText primary="My started games" />
-							</MaterialUI.ListItem>
-							<MaterialUI.ListItem button>
-								<MaterialUI.ListItemText primary="My staging games" />
-							</MaterialUI.ListItem>
-							<MaterialUI.ListItem button>
-								<MaterialUI.ListItemText primary="My finished games" />
-							</MaterialUI.ListItem>
-							<MaterialUI.ListItem button>
-								<MaterialUI.ListItemText primary="Open games" />
-							</MaterialUI.ListItem>
-							<MaterialUI.ListItem button>
-								<MaterialUI.ListItemText primary="Started games" />
-							</MaterialUI.ListItem>
-							<MaterialUI.ListItem button>
-								<MaterialUI.ListItemText primary="Finished games" />
-							</MaterialUI.ListItem>
-						</MaterialUI.List>
-					</div>
+			        <MaterialUI.ClickAwayListener onClickAway={this.closeDrawer}>
+         				<div onClick={this.closeDrawer}>
+         					<MaterialUI.List component="nav">
+         						<MaterialUI.ListItem button>
+         							<MaterialUI.ListItemText primary="Notifications" />
+         						</MaterialUI.ListItem>
+         						<MaterialUI.ListItem button>
+         							<MaterialUI.ListItemText primary="My started games" />
+         						</MaterialUI.ListItem>
+         						<MaterialUI.ListItem button>
+         							<MaterialUI.ListItemText primary="My staging games" />
+         						</MaterialUI.ListItem>
+         						<MaterialUI.ListItem button>
+         							<MaterialUI.ListItemText primary="My finished games" />
+         						</MaterialUI.ListItem>
+         						<MaterialUI.ListItem button>
+         							<MaterialUI.ListItemText primary="Open games" />
+         						</MaterialUI.ListItem>
+         						<MaterialUI.ListItem button>
+         							<MaterialUI.ListItemText primary="Started games" />
+         						</MaterialUI.ListItem>
+         						<MaterialUI.ListItem button>
+         							<MaterialUI.ListItemText primary="Finished games" />
+         						</MaterialUI.ListItem>
+         					</MaterialUI.List>
+         				</div>
+			         </MaterialUI.ClickAwayListener>
 				</MaterialUI.Drawer>
 			</div>
 		);
