b6f6ed9962d7f3718b7ec65904b8bd2440750153
Martin Bruse <zondolfin@gmail.com>
2020-03-30 15:48:58 +0200

    Made the notification component render expansion-tab-wrapped game lists for my started, staging and finished games.

--- resources/js/notifications.js
+++ resources/js/notifications.js
@@ -1,22 +1,61 @@
+import GameList from '%{ cb "/js/game_list.js" }%';
+
 export default class Notifications extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <MaterialUI.List>
-          <MaterialUI.ListItem>
-            <MaterialUI.ListItemText primary="NEWS: Diplicity has a new React-based UI." />
-          </MaterialUI.ListItem>
-          <MaterialUI.ListItem>
-            <MaterialUI.ListItemText primary="MESSAGE: [Fra-Eng] Please stop bouncing in the English Channel." />
-          </MaterialUI.ListItem>
-          <MaterialUI.ListItem>
-            <MaterialUI.ListItemText primary="INFO: No idea how this will be populated yet." />
-          </MaterialUI.ListItem>
-        </MaterialUI.List>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<MaterialUI.List>
+				<MaterialUI.ListItem>
+					<MaterialUI.ListItemText primary="NEWS: Diplicity has a new React-based UI." />
+				</MaterialUI.ListItem>
+				<li key="started">
+					<ul style={{ paddingInlineStart: 0 }}>
+						<MaterialUI.ListSubheader>
+							Started
+						</MaterialUI.ListSubheader>
+						<MaterialUI.ListItem>
+							<GameList
+								expansionPanelWrapped={true}
+								url={this.props.urls["my-started-games"]}
+								limit={8}
+								skipMore={true}
+							/>
+						</MaterialUI.ListItem>
+					</ul>
+				</li>
+				<li key="staging">
+					<ul style={{ paddingInlineStart: 0 }}>
+						<MaterialUI.ListSubheader>
+							Staging
+						</MaterialUI.ListSubheader>
+						<MaterialUI.ListItem>
+							<GameList
+								expansionPanelWrapped={true}
+								url={this.props.urls["my-staging-games"]}
+								limit={8}
+								skipMore={true}
+							/>
+						</MaterialUI.ListItem>
+					</ul>
+				</li>
+				<li key="finished">
+					<ul style={{ paddingInlineStart: 0 }}>
+						<MaterialUI.ListSubheader>
+							Finished
+						</MaterialUI.ListSubheader>
+						<MaterialUI.ListItem>
+							<GameList
+								expansionPanelWrapped={true}
+								url={this.props.urls["my-finished-games"]}
+								limit={8}
+								skipMore={true}
+							/>
+						</MaterialUI.ListItem>
+					</ul>
+				</li>
+			</MaterialUI.List>
+		);
+	}
 }
