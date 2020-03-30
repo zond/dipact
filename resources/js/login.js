33e0985e87e81a1b83260057a01a6ea7c09f9010
Martin Bruse <zondolfin@gmail.com>
2020-03-30 15:47:49 +0200

    Cleaned up the login activity by removing redundant (and Babel-complained-about) <p> tags, and running Prettier.

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,21 +1,35 @@
 export default class Login extends React.Component {
-
 	render() {
 		if (this.props.loginURL) {
 			return (
 				<MaterialUI.Container
-					style={{ textAlign: "center","paddingTop":"50px"}}
+					style={{ textAlign: "center", paddingTop: "50px" }}
 				>
-				<p><MaterialUI.Typography variant="h2">Welcome to DipAct</MaterialUI.Typography></p>
-				<p><MaterialUI.Typography variant="body1">The work-in-progress web version of Diplicity.</MaterialUI.Typography></p>
-					<a href={this.props.loginURL} style={{"textDecoration":"none","textTransform":"none"}}>
+					<MaterialUI.Typography variant="h2">
+						Welcome to DipAct
+					</MaterialUI.Typography>
+					<MaterialUI.Typography variant="body1">
+						The work-in-progress web version of Diplicity.
+					</MaterialUI.Typography>
+					<a
+						href={this.props.loginURL}
+						style={{
+							textDecoration: "none",
+							textTransform: "none"
+						}}
+					>
 						<MaterialUI.Button
 							variant="contained"
-							style={{"backgroundColor":"white", "color":"#757575"}}
+							style={{
+								backgroundColor: "white",
+								color: "#757575"
+							}}
 							startIcon={
-								<i><img src={"/static/img/google_icon.svg"} /> </i>
+								<i>
+									<img src={"/static/img/google_icon.svg"} />{" "}
+								</i>
 							}
-							>
+						>
 							Sign in with Google
 						</MaterialUI.Button>
 					</a>
