64e73017fce81ee9585d5875a5c553f0a7ec68a8
Martin Bruse <zondolfin@gmail.com>
2020-04-04 20:51:34 +0200

    Removed redundant div.

--- resources/js/find_game_dialog.js
+++ resources/js/find_game_dialog.js
@@ -26,45 +26,40 @@ export default class FindGameDialog extends React.Component {
 	}
 	render() {
 		return (
-			<div>
-				<MaterialUI.Dialog
-					open={this.state.open}
-					className="find-game-dialog"
-					disableBackdropClick={false}
-					onClose={this.close}
-				>
-					<MaterialUI.DialogTitle id="form-dialog-title">
-						Find (private) game
-					</MaterialUI.DialogTitle>
-					<MaterialUI.DialogContent>
-						<MaterialUI.DialogContentText>
-							Enter any game ID to view it. You can find the Game
-							ID at [TODO: description wheretofind].
-						</MaterialUI.DialogContentText>
-						<MaterialUI.TextField
-							id="find-game-by-id-input-field"
-							label="Game ID"
-							autoFocus
-							margin="dense"
-							fullWidth
-						/>
-						<MaterialUI.DialogActions>
-							<MaterialUI.Button
-								onClick={this.close}
-								color="primary"
-							>
-								Cancel
-							</MaterialUI.Button>
-							<MaterialUI.Button
-								onClick={this.onClick}
-								color="primary"
-							>
-								Find
-							</MaterialUI.Button>
-						</MaterialUI.DialogActions>
-					</MaterialUI.DialogContent>
-				</MaterialUI.Dialog>
-			</div>
+			<MaterialUI.Dialog
+				open={this.state.open}
+				className="find-game-dialog"
+				disableBackdropClick={false}
+				onClose={this.close}
+			>
+				<MaterialUI.DialogTitle id="form-dialog-title">
+					Find (private) game
+				</MaterialUI.DialogTitle>
+				<MaterialUI.DialogContent>
+					<MaterialUI.DialogContentText>
+						Enter any game ID to view it. You can find the Game ID
+						at [TODO: description wheretofind].
+					</MaterialUI.DialogContentText>
+					<MaterialUI.TextField
+						id="find-game-by-id-input-field"
+						label="Game ID"
+						autoFocus
+						margin="dense"
+						fullWidth
+					/>
+					<MaterialUI.DialogActions>
+						<MaterialUI.Button onClick={this.close} color="primary">
+							Cancel
+						</MaterialUI.Button>
+						<MaterialUI.Button
+							onClick={this.onClick}
+							color="primary"
+						>
+							Find
+						</MaterialUI.Button>
+					</MaterialUI.DialogActions>
+				</MaterialUI.DialogContent>
+			</MaterialUI.Dialog>
 		);
 	}
 }
