4f47d1270811adbce3850ff1321deac5affbb8b7
Martin Bruse <zondolfin@gmail.com>
2020-04-09 22:03:07 +0200

    Made create channel dialog respect disabled channels. Fixes #56.

--- resources/js/create_channel_dialog.js
+++ resources/js/create_channel_dialog.js
@@ -1,3 +1,5 @@
+import * as helpers from '%{ cb "/js/helpers.js" }%';
+
 export default class CreateChannelDialog extends React.Component {
 	constructor(props) {
 		super(props);
@@ -10,7 +12,7 @@ export default class CreateChannelDialog extends React.Component {
 		this.member = this.props.game.Properties.Members.find(e => {
 			return e.User.Email == Globals.user.Email;
 		});
-		this.state = { open: false, members: {} };
+		this.state = { open: false, members: {}, errorMessage: null };
 		this.state.members[this.member.Nation] = true;
 		this.variant = Globals.variants.find(v => {
 			return v.Properties.Name == this.props.game.Properties.Variant;
@@ -21,18 +23,64 @@ export default class CreateChannelDialog extends React.Component {
 		for (let member in this.state.members) {
 			channel.Properties.Members.push(member);
 		}
-		this.close().then(_ => {
-			this.props.createChannel(channel);
-		});
+		let nMembers = Object.keys(this.state.members).length;
+		if (
+			this.props.game.Properties.DisableConferenceChat &&
+			nMembers == this.variant.Properties.Nations.length
+		) {
+			this.setState({
+				errorMessage:
+					"Conference chat is disabled for this game, you can't create a channel with everyone as a member."
+			});
+		} else if (
+			this.props.game.Properties.DisableGroupChat &&
+			nMembers > 2 &&
+			nMembers != this.variant.Properties.Nations.length
+		) {
+			this.setState({
+				errorMessage:
+					"Group chat is disabled for this game, you can't create a channel with more than two, but less than everyone, as members."
+			});
+		} else if (
+			this.props.game.Properties.DisablePrivateChat &&
+			nMembers == 2
+		) {
+			this.setState({
+				errorMessage:
+					"Private chat is disabled for this game, you can't create a channel with two members."
+			});
+		} else {
+			this.close().then(_ => {
+				this.props.createChannel(channel);
+			});
+		}
 	}
 	toggleMember(nation) {
 		return _ => {
 			this.setState((state, props) => {
 				state = Object.assign({}, state);
 				if (state.members[nation]) {
-					delete state.members[nation];
+					if (
+						this.props.game.Properties.DisablePrivateChat &&
+						this.props.game.Properties.DisableGroupChat
+					) {
+						state.members = {};
+						state.members[this.member.Nation] = true;
+					} else {
+						delete state.members[nation];
+					}
 				} else {
-					state.members[nation] = true;
+					if (
+						this.props.game.Properties.DisablePrivateChat &&
+						this.props.game.Properties.DisableGroupChat
+					) {
+						state.members = {};
+						this.variant.Properties.Nations.forEach(nation => {
+							state.members[nation] = true;
+						});
+					} else {
+						state.members[nation] = true;
+					}
 				}
 				return state;
 			});
@@ -84,6 +132,30 @@ export default class CreateChannelDialog extends React.Component {
 						</MaterialUI.Button>
 					</MaterialUI.DialogActions>
 				</MaterialUI.DialogContent>
+				<MaterialUI.Snackbar
+					anchorOrigin={{
+						vertical: "bottom",
+						horizontal: "center"
+					}}
+					open={!!this.state.errorMessage}
+					autoHideDuration={6000}
+					onClose={_ => {
+						this.setState({ errorMessage: null });
+					}}
+					message={this.state.errorMessage}
+					action={
+						<MaterialUI.IconButton
+							size="small"
+							aria-label="close"
+							color="inherit"
+							onClick={_ => {
+								this.setState({ errorMessage: null });
+							}}
+						>
+							{helpers.createIcon("\ue5cd")}
+						</MaterialUI.IconButton>
+					}
+				/>
 			</MaterialUI.Dialog>
 		);
 	}
