94760965bbb796661014736000f56b624835b66c
Joren <7293081+JorenC@users.noreply.github.com>
2020-04-19 11:18:20 +0200

    Update start.js

--- resources/js/start.js
+++ resources/js/start.js
@@ -123,7 +123,7 @@ export default class Start extends React.Component {
 											.newGameFormOpen
 									});
 								}}
-								variant="outlined"
+								variant="contained"
 								color="secondary"
 							>
 								New game
