7eeaebe3f866a7efc5c72946583203ee4e99f4dc
Martin Bruse <zondolfin@gmail.com>
2020-05-15 11:10:37 +0200

    Added a simple deep equal method.

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -470,3 +470,12 @@ export function copyToClipboard(s) {
 		return navigator.clipboard.writeText(s);
 	}
 }
+
+export function deepEqual(x, y) {
+	return x && y && typeof x === "object" && typeof y === "object"
+		? Object.keys(x).length === Object.keys(y).length &&
+				Object.keys(x).reduce(function(isEqual, key) {
+					return isEqual && deepEqual(x[key], y[key]);
+				}, true)
+		: x === y;
+}
