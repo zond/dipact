1603f3e854f7a9ea322410e8574be976f52c4295
Martin Bruse <zondolfin@gmail.com>
2020-06-12 08:24:42 +0200

    Made the short duration view slightly longer in some cases.

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -337,16 +337,20 @@ export function minutesToDuration(m, short = false) {
 			let h = Number.parseInt(m / 60);
 			let remainder = m - h * 60;
 			let rval = "" + h + "h";
-			if (remainder == 0 || short) {
+			if (remainder == 0) {
 				return rval;
+			} else if (short && h > 8) {
+				return ">" + rval;
 			}
 			return rval + " " + reduce(remainder);
 		} else if (m < 60 * 24 * 7) {
 			let d = Number.parseInt(m / (60 * 24));
 			let remainder = m - d * 60 * 24;
 			let rval = "" + d + "d";
-			if (remainder == 0 || short) {
+			if (remainder == 0) {
 				return rval;
+			} else if (short && d > 1) {
+				return ">" + rval;
 			}
 			return rval + " " + reduce(remainder);
 		} else {
