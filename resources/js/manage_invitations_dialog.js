92f2df6b7fe7ca9777146f608d3bd4d3640b8d46
Martin Bruse <zondolfin@gmail.com>
2021-01-13 14:24:28 +0100

    Some work on the invitation management.

--- resources/js/manage_invitations_dialog.js
+++ resources/js/manage_invitations_dialog.js
@@ -5,7 +5,11 @@ export default class ManageInvitationsDialog extends React.Component {
 		super(props);
 		this.state = {
 			open: false,
+			game: this.props.game,
 		};
+		this.variant = Globals.variants.find((variant) => {
+			return variant.Name == this.props.game.Properties.Variant;
+		});
 		if (this.props.parentCB) {
 			this.props.parentCB(this);
 		}
@@ -25,6 +29,9 @@ export default class ManageInvitationsDialog extends React.Component {
 		this.setState({ open: false });
 	}
 	render() {
+		if (!this.state.open) {
+			return "";
+		}
 		return (
 			<MaterialUI.Dialog
 				open={this.state.open}
@@ -35,7 +42,60 @@ export default class ManageInvitationsDialog extends React.Component {
 				<MaterialUI.DialogTitle>
 					Manage invitations
 				</MaterialUI.DialogTitle>
-				<MaterialUI.DialogContent></MaterialUI.DialogContent>
+				<MaterialUI.DialogContent>
+					{this.state.game.Properties.GameMasterInvitations &&
+					this.state.game.Properties.GameMasterInvitations.length >
+						0 ? (
+						<MaterialUI.Typography style={{ margin: "1em" }}>
+							Invited players
+						</MaterialUI.Typography>
+					) : (
+						<MaterialUI.Typography style={{ margin: "1em" }}>
+							Nobody invited yet
+						</MaterialUI.Typography>
+					)}
+					<MaterialUI.TextField
+						key="Desc"
+						label="Email"
+						margin="dense"
+						style={{
+							marginBottom: "8px",
+							flexGrow: "1",
+						}}
+					/>
+					<MaterialUI.InputLabel
+						shrink
+						id="nationlabel"
+						style={{
+							marginTop: "16px",
+						}}
+					>
+						Nation
+					</MaterialUI.InputLabel>
+					<MaterialUI.Select
+						key="Nation"
+						labelId="nationlabel"
+						value="normal_allocation"
+						style={{ marginBottom: "16px" }}
+					>
+						<MaterialUI.MenuItem
+							key="normal_allocation"
+							value="normal_allocation"
+						>
+							Normal allocation
+						</MaterialUI.MenuItem>
+						{this.variant.Properties.Nations.map((nation) => {
+							return (
+								<MaterialUI.MenuItem
+									key={nation}
+									value={nation}
+								>
+									{nation}
+								</MaterialUI.MenuItem>
+							);
+						})}
+					</MaterialUI.Select>
+				</MaterialUI.DialogContent>
 			</MaterialUI.Dialog>
 		);
 	}
