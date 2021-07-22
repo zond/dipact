9d6692f06fe77fff143221b4a8fc8476029b0e45
John McDowell <johnmcdowell@microsoft.com>
2021-07-22 17:05:19 +0100

    Add redirect server

---
+++ web-app/src/redirectServer.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const port = process.env.PORT || 4000;
+const router = express.Router();
+const app = express();
+
+const indexRouter = router.post('/', function(req, res, next) {
+    res.redirect('http://localhost:3002');  // <- need to get actual url of react app
+});
+
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+});
