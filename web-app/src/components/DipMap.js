fa802c9e8a0ee7e6b21ffcdc85829d28b27a5431
Martin Bruse <zondolfin@gmail.com>
2021-08-18 23:27:54 +0200

    Backported the createImageBitmap change.

--- web-app/src/components/DipMap.js
+++ web-app/src/components/DipMap.js
@@ -289,13 +289,32 @@ export default class DipMap extends React.Component {
 					"data:image/svg+xml;base64," + serializedSVG;
 				snapshotImage.addEventListener("load", (_) => {
 					this.debugCount("getSVGData/loadedSnapshot");
-					createImageBitmap(
-						snapshotImage,
-						0,
-						0,
-						snapshotImage.width,
-						snapshotImage.height
-					).then((bitmap) => {
+					if ("createImageBitmap" in window) {
+						createImageBitmap(
+							snapshotImage,
+							0,
+							0,
+							snapshotImage.width,
+							snapshotImage.height
+						).then((bitmap) => {
+							const snapshotCanvas =
+								document.createElement("canvas");
+							snapshotCanvas.setAttribute(
+								"height",
+								this.mapDims[1] * scale
+							);
+							snapshotCanvas.setAttribute(
+								"width",
+								this.mapDims[0] * scale
+							);
+							snapshotCanvas.style.height = this.mapDims[1];
+							snapshotCanvas.style.width = this.mapDims[0];
+							snapshotCanvas
+								.getContext("bitmaprenderer")
+								.transferFromImageBitmap(bitmap);
+							res(snapshotCanvas.toDataURL("image/png"));
+						});
+					} else {
 						const snapshotCanvas = document.createElement("canvas");
 						snapshotCanvas.setAttribute(
 							"height",
@@ -308,10 +327,10 @@ export default class DipMap extends React.Component {
 						snapshotCanvas.style.height = this.mapDims[1];
 						snapshotCanvas.style.width = this.mapDims[0];
 						snapshotCanvas
-							.getContext("bitmaprenderer")
-							.transferFromImageBitmap(bitmap);
+							.getContext("2d")
+							.drawImage(snapshotImage, 0, 0);
 						res(snapshotCanvas.toDataURL("image/png"));
-					});
+					}
 				});
 			}
 		});
