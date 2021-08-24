cb4424f983b3f66936065382bf75a5db6849c5aa
j.bredman@gmail.com <j.bredman@gmail.com>
2021-08-24 09:14:54 +0200

    Update GameListElement.js
    I've created a new, better style Language display.
    
    However, I can't for the life of me get this box aligned with the parent top...
    Do you see the mistake?

--- web-app/src/components/GameListElement.js
+++ web-app/src/components/GameListElement.js
@@ -1,56 +1,79 @@
 /* eslint-disable no-restricted-globals */
-import React from 'react';
-import * as helpers from '../helpers';
-import gtag from 'ga-gtag';
-import {SvgIcon, Divider, Typography, Tooltip, Box, Button, Badge, Zoom, AccordionSummary, Accordion, AccordionDetails } from "@material-ui/core";
+import React from "react";
+import * as helpers from "../helpers";
+import gtag from "ga-gtag";
+import {
+	SvgIcon,
+	Divider,
+	Typography,
+	Tooltip,
+	Box,
+	Button,
+	Badge,
+	Zoom,
+	AccordionSummary,
+	Accordion,
+	AccordionDetails,
+} from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 
-import GameMetadata from './GameMetadata';
-import Game from './Game';
-import Globals from '../Globals';
-import NationPreferencesDialog from './NationPreferencesDialog';
-import RenameGameDialog from './RenameGameDialog';
-import ManageInvitationsDialog from './ManageInvitationsDialog';
-import RescheduleDialog from './RescheduleDialog';
+import GameMetadata from "./GameMetadata";
+import Game from "./Game";
+import Globals from "../Globals";
+import NationPreferencesDialog from "./NationPreferencesDialog";
+import RenameGameDialog from "./RenameGameDialog";
+import ManageInvitationsDialog from "./ManageInvitationsDialog";
+import RescheduleDialog from "./RescheduleDialog";
 
-import { ConfirmedReadyIcon, ExpandIcon, GavelIcon, MusteringIcon, NationAllocationIcon, NumMembersIcon, PrivateGameIcon, RatingIcon, ReliabilityIcon, StartedAtIcon } from '../icons';
+import {
+	ConfirmedReadyIcon,
+	ExpandIcon,
+	GavelIcon,
+	MusteringIcon,
+	NationAllocationIcon,
+	NumMembersIcon,
+	PrivateGameIcon,
+	RatingIcon,
+	ReliabilityIcon,
+	StartedAtIcon,
+} from "../icons";
 
-const warningClass = {color: "red"};
+const warningClass = { color: "red" };
 const noticeClass = {
-	fontWeight: "bold !important"
+	fontWeight: "bold !important",
 };
 const secondRowSummaryClass = {
-	display: 'flex',
-	flexDirection: 'row',
-	justifyContent: 'space-between',
-	flexWrap: 'wrap',
-}
+	display: "flex",
+	flexDirection: "row",
+	justifyContent: "space-between",
+	flexWrap: "wrap",
+};
 const secondRowSummaryColorClass = {
-	color: "rgba(40, 26, 26, 0.7)"
+	color: "rgba(40, 26, 26, 0.7)",
 };
 const summaryIconsAndPhaseClass = {
-	display: 'flex',
-	justifyContent: 'right',
-}
+	display: "flex",
+	justifyContent: "right",
+};
 const summaryIconsClass = {
-	paddingRight: "4px"
-}
+	paddingRight: "4px",
+};
 
 const styles = (theme) => ({
-  accordionSummaryRoot: {
-	padding: theme.spacing(0),
-  },
-  accordionSummaryContent: {
-	maxWidth: `calc(100% - ${theme.spacing(4)}px)`,
-  },
-  accordionDetails: {
-	padding: theme.spacing(0),
-	maxWidth: `calc(100% - ${theme.spacing(4)})`,
-  },
-  sixteenBySixteenClass: {
-	height: '16px !important',
-	width: '16px !important',
-  }
+	accordionSummaryRoot: {
+		padding: theme.spacing(0),
+	},
+	accordionSummaryContent: {
+		maxWidth: `calc(100% - ${theme.spacing(4)}px)`,
+	},
+	accordionDetails: {
+		padding: theme.spacing(0),
+		maxWidth: `calc(100% - ${theme.spacing(4)})`,
+	},
+	sixteenBySixteenClass: {
+		height: "16px !important",
+		width: "16px !important",
+	},
 });
 
 class GameListElement extends React.Component {
@@ -281,11 +304,13 @@ class GameListElement extends React.Component {
 				disableFocusListener
 				title={tooltip}
 			>
-				<Icon style={{
-					padding: "4px 1px 0px 1px",
-					color: color,
-					fontSize: "14px",
-				}} />
+				<Icon
+					style={{
+						padding: "4px 1px 0px 1px",
+						color: color,
+						fontSize: "14px",
+					}}
+				/>
 			</Tooltip>
 		);
 	}
@@ -293,12 +318,7 @@ class GameListElement extends React.Component {
 		let itemKey = 0;
 		let icons = [];
 		if (this.state.game.Properties.GameMasterEnabled) {
-			this.addIconWithTooltip(
-				icons,
-				GavelIcon,
-				"black",
-				"Game master present"
-			);
+			this.addIconWithTooltip(icons, GavelIcon, "black", "Game master present");
 		}
 		if (this.state.game.Properties.ChatLanguageISO639_1) {
 			icons.push(
@@ -309,15 +329,39 @@ class GameListElement extends React.Component {
 						"Chat language: " +
 						helpers.iso639_1Codes.find((el) => {
 							return (
-								el.code ===
-								this.state.game.Properties.ChatLanguageISO639_1
+								el.code === this.state.game.Properties.ChatLanguageISO639_1
 							);
 						}).name
 					}
 				>
+					<div
+						style={{
+							fontFamily: "cabin",
+							height: "16px",
+							width: "16px",
+							display: "inline-flex",
+							alignSelf: "flex-start",
+							backgroundImage:
+								'url(\'data:image/svg+xml;utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18" viewBox="0 0 24 24"><path fill="%23000000" d="M 20 2 H 4 c -1.1 0 -2 0.9 -2 2 v 18 l 4 -4 h 14 c 1.1 0 2 -0.9 2 -2 V 4 c 0 -1.1 -0.9 -2 -2 -2 Z"></path></svg>\')',
+						}}
+					>
+						<div
+							style={{
+								fontSize: "10px",
+								color: "#FDE2B5",
+								marginTop: "0px",
+								padding: "0px 0px 0px 3px",
+								align: "center",
+							}}
+						>
+							{this.state.game.Properties.ChatLanguageISO639_1}
+						</div>
+					</div>
+					{/*}
 					<Typography variant={'body2'} className={`${this.props.classes.sixteenBySixteenClass} speech-bubble`} style={{ display: "inline" }}>
 						{this.state.game.Properties.ChatLanguageISO639_1}
 					</Typography>
+				*/}
 				</Tooltip>
 			);
 		}
@@ -391,11 +435,7 @@ class GameListElement extends React.Component {
 			this.state.game.Properties.DisablePrivateChat
 		) {
 			icons.push(
-				<Tooltip
-					key={itemKey++}
-					disableFocusListener
-					title="Chat disabled"
-				>
+				<Tooltip key={itemKey++} disableFocusListener title="Chat disabled">
 					<SvgIcon className={this.props.classes.sixteenBySixteenClass}>
 						<g
 							id="Artboard"
@@ -405,10 +445,7 @@ class GameListElement extends React.Component {
 							fillRule="evenodd"
 						>
 							<g id="message-24px">
-								<polygon
-									id="Path"
-									points="0 0 24 0 24 24 0 24"
-								></polygon>
+								<polygon id="Path" points="0 0 24 0 24 24 0 24"></polygon>
 								<path
 									d="M2.4,2.614 L20.5847763,20.7989899 L20.598,20.784 L20.7989899,20.9842712 L19.3847763,22.3984848 L14.986,18 L6,18 L2,22 L2.008,5.022 L1,4.0137085 L2.4,2.614 Z M20,2 C21.05,2 21.9177686,2.82004132 21.9944872,3.85130541 L22,4 L22,16 C22,16.9134058 21.3794387,17.6889091 20.539101,17.925725 L16.614,14 L18,14 L18,12 L14.614,12 L13.614,11 L18,11 L18,9 L11.614,9 L10.614,8 L18,8 L18,6 L8.614,6 L4.614,2 L20,2 Z M8.987,12 L6,12 L6,14 L10.986,14 L8.987,12 Z M6,9.013 L6,11 L7.987,11 L6,9.013 Z"
 									id="Combined-Shape"
@@ -455,10 +492,7 @@ class GameListElement extends React.Component {
 			this.state.game.Properties.ActiveBans.length > 0
 		) {
 			buttons.push(
-				<Typography
-					key="banned-notice"
-					style={noticeClass}
-				>
+				<Typography key="banned-notice" style={noticeClass}>
 					You can't join because you banned or are banned by a player.
 				</Typography>
 			);
@@ -468,16 +502,11 @@ class GameListElement extends React.Component {
 			this.state.game.Properties.FailedRequirements
 		) {
 			buttons.push(
-				<Typography
-					key="requirement-notice"
-					style={noticeClass}
-				>
+				<Typography key="requirement-notice" style={noticeClass}>
 					You can't join this game because:{" "}
-					{this.state.game.Properties.FailedRequirements.map(
-						(req) => {
-							return this.failureExplanations()[req];
-						}
-					).join(" ")}
+					{this.state.game.Properties.FailedRequirements.map((req) => {
+						return this.failureExplanations()[req];
+					}).join(" ")}
 				</Typography>
 			);
 		}
@@ -559,21 +588,14 @@ class GameListElement extends React.Component {
 			if (link.Rel === "join") {
 				if (
 					this.state.game.Properties.PhaseLengthMinutes < 60 * 12 ||
-					(this.state.game.Properties.NonMovementPhaseLengthMinutes !==
-						0 &&
-						this.state.game.Properties
-							.NonMovementPhaseLengthMinutes <
-							60 * 12)
+					(this.state.game.Properties.NonMovementPhaseLengthMinutes !== 0 &&
+						this.state.game.Properties.NonMovementPhaseLengthMinutes < 60 * 12)
 				) {
 					buttons.unshift(
-						<Typography
-							key="deadline-warning"
-							style={warningClass}
-						>
-							WARNING: This game has short deadlines (less than 12
-							hours). If it starts while you're unavailable, you
-							might miss parts of the game greatly impacting your
-							reliability score.
+						<Typography key="deadline-warning" style={warningClass}>
+							WARNING: This game has short deadlines (less than 12 hours). If it
+							starts while you're unavailable, you might miss parts of the game
+							greatly impacting your reliability score.
 						</Typography>
 					);
 				}
@@ -583,14 +605,10 @@ class GameListElement extends React.Component {
 					Globals.userStats.Properties.Reliability >= 10
 				) {
 					buttons.unshift(
-						<Typography
-							key="reliability-warning"
-							style={warningClass}
-						>
-							WARNING: We advise you to join a different game,
-							because you have high reliability. Since this game
-							has no reliability requirements, it might have
-							(some) absent players.
+						<Typography key="reliability-warning" style={warningClass}>
+							WARNING: We advise you to join a different game, because you have
+							high reliability. Since this game has no reliability requirements,
+							it might have (some) absent players.
 						</Typography>
 					);
 				}
@@ -722,13 +740,10 @@ class GameListElement extends React.Component {
 										justifyContent: "space-between",
 									}}
 								>
-									{this.state.member &&
-									this.state.member.UnreadMessages > 0 ? (
+									{this.state.member && this.state.member.UnreadMessages > 0 ? (
 										<Badge
 											key={itemKey++}
-											badgeContent={
-												this.state.member.UnreadMessages
-											}
+											badgeContent={this.state.member.UnreadMessages}
 											color="primary"
 											style={{
 												maxWidth: "calc(100% - 70px)",
@@ -738,13 +753,10 @@ class GameListElement extends React.Component {
 												textroverflow="ellipsis"
 												noWrap
 												style={{
-													color:
-														"rgba(40, 26, 26, 1)",
+													color: "rgba(40, 26, 26, 1)",
 												}}
 											>
-												{helpers.gameDesc(
-													this.state.game
-												)}
+												{helpers.gameDesc(this.state.game)}
 											</Typography>
 										</Badge>
 									) : (
@@ -774,8 +786,7 @@ class GameListElement extends React.Component {
 										{this.state.member &&
 										this.state.game.Properties.Started &&
 										!this.state.game.Properties.Finished ? (
-											this.state.member.NewestPhaseState
-												.OnProbation ? (
+											this.state.member.NewestPhaseState.OnProbation ? (
 												<SvgIcon>
 													<path
 														d="M2.98188996,2.24133335 L21.3666663,20.6261097 L20.0261097,21.9666663 L19.0573333,20.998 L19,21 L5,21 C3.95,21 3.0822314,20.1799587 3.00551277,19.1486946 L3,19 L3,5 L3.00233335,4.942 L1.64133335,3.58188996 L2.98188996,2.24133335 Z M12,1 C13.235,1 14.2895,1.7581 14.75196,2.828465 L14.82,3 L19,3 C20.05,3 20.9177686,3.82004132 20.9944872,4.85130541 L21,5 L21,17.963 L16.037,13 L17,13 L17,11 L14.037,11 L12.037,9 L17,9 L17,7 L10.037,7 L6.037,3 L9.18,3 C9.579,1.898 10.5917,1.0848 11.80656,1.006235 L12,1 Z M13.0593333,15 L7,15 L7,17 L15.0593333,17 L13.0593333,15 Z M11.0593333,13 L9.06033335,11 L7,11 L7,13 L11.0593333,13 Z M12,3 C11.45,3 11,3.45 11,4 C11,4.55 11.45,5 12,5 C12.55,5 13,4.55 13,4 C13,3.45 12.55,3 12,3 Z"
@@ -784,9 +795,7 @@ class GameListElement extends React.Component {
 														fillRule="nonzero"
 													></path>
 												</SvgIcon>
-											) : this.state.member
-													.NewestPhaseState
-													.ReadyToResolve ? (
+											) : this.state.member.NewestPhaseState.ReadyToResolve ? (
 												<SvgIcon>
 													<path
 														d="M9,0 C10.3,0 11.4,0.84 11.82,2 L11.82,2 L16,2 C17.1045695,2 18,2.8954305 18,4 L18,4 L18,18 C18,19.1045695 17.1045695,20 16,20 L16,20 L2,20 C0.8954305,20 0,19.1045695 0,18 L0,18 L0,4 C0,2.8954305 0.8954305,2 2,2 L2,2 L6.18,2 C6.6,0.84 7.7,0 9,0 Z M13.4347826,7 L7.70608696,12.7391304 L4.56521739,9.60869565 L3,11.173913 L7.70608696,15.8695652 L15,8.56521739 L13.4347826,7 Z M9,2 C8.44771525,2 8,2.44771525 8,3 C8,3.55228475 8.44771525,4 9,4 C9.55228475,4 10,3.55228475 10,3 C10,2.44771525 9.55228475,2 9,2 Z"
@@ -809,17 +818,13 @@ class GameListElement extends React.Component {
 										>
 											{this.state.game.Properties.Finished
 												? helpers.minutesToDuration(
-														-this.state.game
-															.Properties
-															.FinishedAgo /
+														-this.state.game.Properties.FinishedAgo /
 															1000000000 /
 															60,
 														true
 												  )
 												: helpers.minutesToDuration(
-														this.state.game
-															.Properties
-															.NewestPhaseMeta[0]
+														this.state.game.Properties.NewestPhaseMeta[0]
 															.NextDeadlineIn /
 															1000000000 /
 															60,
@@ -828,10 +833,7 @@ class GameListElement extends React.Component {
 										</Typography>
 									</div>
 								</div>
-								<div
-									key={itemKey++}
-									style={secondRowSummaryClass}
-								>
+								<div key={itemKey++} style={secondRowSummaryClass}>
 									<Typography
 										textroverflow="ellipsis"
 										noWrap={true}
@@ -841,47 +843,29 @@ class GameListElement extends React.Component {
 										style={secondRowSummaryColorClass}
 									>
 										{this.state.game.Properties.Variant}{" "}
-										{helpers.phaseLengthDisplay(
-											this.state.game.Properties
-										)}
+										{helpers.phaseLengthDisplay(this.state.game.Properties)}
 									</Typography>
 									<div style={summaryIconsAndPhaseClass}>
-										<div style={summaryIconsClass}>
-											{this.getIcons()}
-										</div>
+										<div style={summaryIconsClass}>{this.getIcons()}</div>
 										<Typography
 											variant="caption"
-											style={
-												secondRowSummaryColorClass
-											}
+											style={secondRowSummaryColorClass}
 										>
-											{
-												this.state.game.Properties
-													.NewestPhaseMeta[0].Season
-											}{" "}
-											{
-												this.state.game.Properties
-													.NewestPhaseMeta[0].Year
-											}
-											,{" "}
-											{
-												this.state.game.Properties
-													.NewestPhaseMeta[0].Type
-											}
+											{this.state.game.Properties.NewestPhaseMeta[0].Season}{" "}
+											{this.state.game.Properties.NewestPhaseMeta[0].Year},{" "}
+											{this.state.game.Properties.NewestPhaseMeta[0].Type}
 										</Typography>
 									</div>
 								</div>
-								{!this.state.member ||
-								this.state.game.Properties.Mustered ? (
+								{!this.state.member || this.state.game.Properties.Mustered ? (
 									""
-								) : (
-										this.state.game.Properties.Members || []
-								  ).find((m) => {
+								) : (this.state.game.Properties.Members || []).find((m) => {
 										return m.User.Id === Globals.user.Id;
 								  }).NewestPhaseState.ReadyToResolve ? (
-									<Typography className={this.props.classes.sixteenBySixteenClass}>
-										Confirmed ready{" "}
-										<ConfirmedReadyIcon />
+									<Typography
+										className={this.props.classes.sixteenBySixteenClass}
+									>
+										Confirmed ready <ConfirmedReadyIcon />
 									</Typography>
 								) : (
 									<Button
@@ -898,25 +882,19 @@ class GameListElement extends React.Component {
 												.safeFetch(
 													helpers.createRequest(
 														"/Game/" +
-															this.state.game
-																.Properties.ID +
+															this.state.game.Properties.ID +
 															"/Phase/" +
-															this.state.game
-																.Properties
-																.NewestPhaseMeta[0]
+															this.state.game.Properties.NewestPhaseMeta[0]
 																.PhaseOrdinal +
 															"/PhaseState",
 														{
 															headers: {
-																"Content-Type":
-																	"application/json",
+																"Content-Type": "application/json",
 															},
 															method: "PUT",
-															body: JSON.stringify(
-																{
-																	ReadyToResolve: true,
-																}
-															),
+															body: JSON.stringify({
+																ReadyToResolve: true,
+															}),
 														}
 													)
 												)
@@ -959,19 +937,9 @@ class GameListElement extends React.Component {
 										}}
 									>
 										<NumMembersIcon />
-										<Typography
-											variant="body2"
-											style={{ paddingLeft: "2px" }}
-										>
-											{
-												this.state.game.Properties
-													.NMembers
-											}
-											/
-											{
-												this.variant.Properties.Nations
-													.length
-											}{" "}
+										<Typography variant="body2" style={{ paddingLeft: "2px" }}>
+											{this.state.game.Properties.NMembers}/
+											{this.variant.Properties.Nations.length}{" "}
 										</Typography>
 									</div>
 								</div>
@@ -992,9 +960,7 @@ class GameListElement extends React.Component {
 										}}
 									>
 										{this.state.game.Properties.Variant}{" "}
-										{helpers.phaseLengthDisplay(
-											this.state.game.Properties
-										)}
+										{helpers.phaseLengthDisplay(this.state.game.Properties)}
 									</Typography>
 									<div> {this.getIcons()}</div>
 								</div>
@@ -1008,11 +974,7 @@ class GameListElement extends React.Component {
 		);
 
 		let gameView = (
-			<Zoom
-				in={this.state.viewOpen}
-				mountOnEnter
-				unmountOnExit
-			>
+			<Zoom in={this.state.viewOpen} mountOnEnter unmountOnExit>
 				<div
 					style={{
 						position: "fixed",
@@ -1084,14 +1046,15 @@ class GameListElement extends React.Component {
 					}}
 				>
 					<AccordionSummary
-						classes={{ root: classes.accordionSummaryRoot, content: classes.accordionSummaryContent }}
+						classes={{
+							root: classes.accordionSummaryRoot,
+							content: classes.accordionSummaryContent,
+						}}
 						expandIcon={<ExpandIcon />}
 					>
 						{summary}
 					</AccordionSummary>
-					<AccordionDetails
-						className={classes.accordionDetails}
-					>
+					<AccordionDetails className={classes.accordionDetails}>
 						{this.state.expanded ? (
 							<div>
 								<div
@@ -1112,11 +1075,7 @@ class GameListElement extends React.Component {
 									>
 										{buttonDiv}
 										{this.state.dead ? (
-											<Typography
-												style={{ color: "red" }}
-											>
-												Deleted
-											</Typography>
+											<Typography style={{ color: "red" }}>Deleted</Typography>
 										) : (
 											""
 										)}
