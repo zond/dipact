fa01d438c444f54c0bfc0fa2210d4dd62793a399
j.bredman@gmail.com <j.bredman@gmail.com>
2021-09-12 15:54:58 +0200

    Added Native Share
    Made the invite - especially on mobile - easier to use a native functionality.
    
    Pushed to test on other devices and in the wrapper.

--- web-app/src/components/GameListElement.js
+++ web-app/src/components/GameListElement.js
@@ -111,6 +111,7 @@ class GameListElement extends React.Component {
 		this.phaseMessageHandler = this.phaseMessageHandler.bind(this);
 		this.messageHandler = this.messageHandler.bind(this);
 		this.addIconWithTooltip = this.addIconWithTooltip.bind(this);
+		this.shareNative = this.shareNative.bind(this);
 	}
 	onRescheduleSubmit(minutes) {
 		const link = this.state.game.Links.find((link) => {
@@ -135,6 +136,72 @@ class GameListElement extends React.Component {
 				gtag("event", "game_list_element_reschedule");
 				this.reloadGame();
 			});
+	}
+	shareNative() {
+		console.log("shared");
+		const hrefURL = new URL(location.href);
+
+		if (navigator.share) {
+			console.log("support");
+
+			navigator
+				.share({
+					url:
+						hrefURL.protocol +
+						"//" +
+						hrefURL.host +
+						"/Game/" +
+						this.state.game.Properties.ID,
+					title: "Join my game at Diplicity!",
+					text: "Hi! I'd like you to join a game of Diplomacy. Follow the link, log in with Google and press join to play the game.", //If you want to understand the rules, visit http://rules.diplicity.com
+				})
+				.then(() => {
+					console.log("Shared successfull");
+				})
+				.catch(() => {
+					console.log("Shared fail");
+					helpers
+						.copyToClipboard(
+							hrefURL.protocol +
+								"//" +
+								hrefURL.host +
+								"/Game/" +
+								this.state.game.Properties.ID
+						)
+						.then(
+							(_) => {
+								helpers.snackbar(
+									"Game URL copied to clipboard. Share it to other players."
+								);
+							},
+							(err) => {
+								console.log(err);
+							}
+						);
+					gtag("event", "game_share");
+				});
+		} else {
+			console.log("No support for WebAPI, using snackbar");
+			helpers
+				.copyToClipboard(
+					hrefURL.protocol +
+						"//" +
+						hrefURL.host +
+						"/Game/" +
+						this.state.game.Properties.ID
+				)
+				.then(
+					(_) => {
+						helpers.snackbar(
+							"Game URL copied to clipboard. Share it to other players."
+						);
+					},
+					(err) => {
+						console.log(err);
+					}
+				);
+			gtag("event", "game_share");
+		}
 	}
 	reschedule() {
 		this.rescheduleDialog.setState({ open: true });
@@ -318,12 +385,7 @@ class GameListElement extends React.Component {
 		let itemKey = 0;
 		let icons = [];
 		if (this.state.game.Properties.GameMasterEnabled) {
-			this.addIconWithTooltip(
-				icons,
-				GavelIcon,
-				"black",
-				"Game master present"
-			);
+			this.addIconWithTooltip(icons, GavelIcon, "black", "Game master present");
 		}
 		if (this.state.game.Properties.ChatLanguageISO639_1) {
 			icons.push(
@@ -334,8 +396,7 @@ class GameListElement extends React.Component {
 						"Chat language: " +
 						helpers.iso639_1Codes.find((el) => {
 							return (
-								el.code ===
-								this.state.game.Properties.ChatLanguageISO639_1
+								el.code === this.state.game.Properties.ChatLanguageISO639_1
 							);
 						}).name
 					}
@@ -413,9 +474,7 @@ class GameListElement extends React.Component {
 					disableFocusListener
 					title="Maximum hate requirement"
 				>
-					<SvgIcon
-						className={this.props.classes.sixteenBySixteenClass}
-					>
+					<SvgIcon className={this.props.classes.sixteenBySixteenClass}>
 						<g
 							id="Artboard"
 							stroke="none"
@@ -445,14 +504,8 @@ class GameListElement extends React.Component {
 			this.state.game.Properties.DisablePrivateChat
 		) {
 			icons.push(
-				<Tooltip
-					key={itemKey++}
-					disableFocusListener
-					title="Chat disabled"
-				>
-					<SvgIcon
-						className={this.props.classes.sixteenBySixteenClass}
-					>
+				<Tooltip key={itemKey++} disableFocusListener title="Chat disabled">
+					<SvgIcon className={this.props.classes.sixteenBySixteenClass}>
 						<g
 							id="Artboard"
 							stroke="none"
@@ -461,10 +514,7 @@ class GameListElement extends React.Component {
 							fillRule="evenodd"
 						>
 							<g id="message-24px">
-								<polygon
-									id="Path"
-									points="0 0 24 0 24 24 0 24"
-								></polygon>
+								<polygon id="Path" points="0 0 24 0 24 24 0 24"></polygon>
 								<path
 									d="M2.4,2.614 L20.5847763,20.7989899 L20.598,20.784 L20.7989899,20.9842712 L19.3847763,22.3984848 L14.986,18 L6,18 L2,22 L2.008,5.022 L1,4.0137085 L2.4,2.614 Z M20,2 C21.05,2 21.9177686,2.82004132 21.9944872,3.85130541 L22,4 L22,16 C22,16.9134058 21.3794387,17.6889091 20.539101,17.925725 L16.614,14 L18,14 L18,12 L14.614,12 L13.614,11 L18,11 L18,9 L11.614,9 L10.614,8 L18,8 L18,6 L8.614,6 L4.614,2 L20,2 Z M8.987,12 L6,12 L6,14 L10.986,14 L8.987,12 Z M6,9.013 L6,11 L7.987,11 L6,9.013 Z"
 									id="Combined-Shape"
@@ -478,12 +528,7 @@ class GameListElement extends React.Component {
 			);
 		}
 		if (this.state.game.Properties.Private) {
-			this.addIconWithTooltip(
-				icons,
-				PrivateGameIcon,
-				"black",
-				"Private game"
-			);
+			this.addIconWithTooltip(icons, PrivateGameIcon, "black", "Private game");
 		}
 		if (this.state.game.Properties.NationAllocation === 1) {
 			this.addIconWithTooltip(
@@ -528,11 +573,9 @@ class GameListElement extends React.Component {
 			buttons.push(
 				<Typography key="requirement-notice" style={noticeClass}>
 					You can't join this game because:{" "}
-					{this.state.game.Properties.FailedRequirements.map(
-						(req) => {
-							return this.failureExplanations()[req];
-						}
-					).join(" ")}
+					{this.state.game.Properties.FailedRequirements.map((req) => {
+						return this.failureExplanations()[req];
+					}).join(" ")}
 				</Typography>
 			);
 		}
@@ -581,7 +624,36 @@ class GameListElement extends React.Component {
 						marginBottom: "4px",
 					}}
 					color="primary"
-					onClick={(_) => {
+					onClick={
+						this.shareNative
+						/*const canonical = document.querySelector("link[rel=canonical]");
+		let url = canonical ? canonical.href : document.location.href;
+		const shareDetails = { url, title, text };
+
+		console.log(shareDetails);
+
+		const handleSharing = async () => {
+			//check if sharer is present, use that.
+			if (navigator.share) {
+				try {
+					await navigator
+						.share(shareDetails)
+						.then(() => console.log("Hooray, content shared!"));
+				} catch (error) {
+					console.log('oops, error ${error}');
+				}
+			} else {
+				//fallback if no sharer feature.
+				console.log('not supported');
+			}
+		};
+	}
+
+
+
+*/
+					}
+					/* onClick={(_) => {
 						const hrefURL = new URL(location.href);
 						helpers
 							.copyToClipboard(
@@ -602,7 +674,7 @@ class GameListElement extends React.Component {
 								}
 							);
 						gtag("event", "game_share");
-					}}
+					}} */
 					key={itemKey++}
 				>
 					Invite
@@ -614,18 +686,14 @@ class GameListElement extends React.Component {
 			if (link.Rel === "join") {
 				if (
 					this.state.game.Properties.PhaseLengthMinutes < 60 * 12 ||
-					(this.state.game.Properties
-						.NonMovementPhaseLengthMinutes !== 0 &&
-						this.state.game.Properties
-							.NonMovementPhaseLengthMinutes <
-							60 * 12)
+					(this.state.game.Properties.NonMovementPhaseLengthMinutes !== 0 &&
+						this.state.game.Properties.NonMovementPhaseLengthMinutes < 60 * 12)
 				) {
 					buttons.unshift(
 						<Typography key="deadline-warning" style={warningClass}>
-							WARNING: This game has short deadlines (less than 12
-							hours). If it starts while you're unavailable, you
-							might miss parts of the game greatly impacting your
-							reliability score.
+							WARNING: This game has short deadlines (less than 12 hours). If it
+							starts while you're unavailable, you might miss parts of the game
+							greatly impacting your reliability score.
 						</Typography>
 					);
 				}
@@ -635,14 +703,10 @@ class GameListElement extends React.Component {
 					Globals.userStats.Properties.Reliability >= 10
 				) {
 					buttons.unshift(
-						<Typography
-							key="reliability-warning"
-							style={warningClass}
-						>
-							WARNING: We advise you to join a different game,
-							because you have high reliability. Since this game
-							has no reliability requirements, it might have
-							(some) absent players.
+						<Typography key="reliability-warning" style={warningClass}>
+							WARNING: We advise you to join a different game, because you have
+							high reliability. Since this game has no reliability requirements,
+							it might have (some) absent players.
 						</Typography>
 					);
 				}
@@ -774,13 +838,10 @@ class GameListElement extends React.Component {
 										justifyContent: "space-between",
 									}}
 								>
-									{this.state.member &&
-									this.state.member.UnreadMessages > 0 ? (
+									{this.state.member && this.state.member.UnreadMessages > 0 ? (
 										<Badge
 											key={itemKey++}
-											badgeContent={
-												this.state.member.UnreadMessages
-											}
+											badgeContent={this.state.member.UnreadMessages}
 											color="primary"
 											style={{
 												maxWidth: "calc(100% - 70px)",
@@ -793,9 +854,7 @@ class GameListElement extends React.Component {
 													color: "rgba(40, 26, 26, 1)",
 												}}
 											>
-												{helpers.gameDesc(
-													this.state.game
-												)}
+												{helpers.gameDesc(this.state.game)}
 											</Typography>
 										</Badge>
 									) : (
@@ -825,8 +884,7 @@ class GameListElement extends React.Component {
 										{this.state.member &&
 										this.state.game.Properties.Started &&
 										!this.state.game.Properties.Finished ? (
-											this.state.member.NewestPhaseState
-												.OnProbation ? (
+											this.state.member.NewestPhaseState.OnProbation ? (
 												<SvgIcon>
 													<path
 														d="M2.98188996,2.24133335 L21.3666663,20.6261097 L20.0261097,21.9666663 L19.0573333,20.998 L19,21 L5,21 C3.95,21 3.0822314,20.1799587 3.00551277,19.1486946 L3,19 L3,5 L3.00233335,4.942 L1.64133335,3.58188996 L2.98188996,2.24133335 Z M12,1 C13.235,1 14.2895,1.7581 14.75196,2.828465 L14.82,3 L19,3 C20.05,3 20.9177686,3.82004132 20.9944872,4.85130541 L21,5 L21,17.963 L16.037,13 L17,13 L17,11 L14.037,11 L12.037,9 L17,9 L17,7 L10.037,7 L6.037,3 L9.18,3 C9.579,1.898 10.5917,1.0848 11.80656,1.006235 L12,1 Z M13.0593333,15 L7,15 L7,17 L15.0593333,17 L13.0593333,15 Z M11.0593333,13 L9.06033335,11 L7,11 L7,13 L11.0593333,13 Z M12,3 C11.45,3 11,3.45 11,4 C11,4.55 11.45,5 12,5 C12.55,5 13,4.55 13,4 C13,3.45 12.55,3 12,3 Z"
@@ -835,9 +893,7 @@ class GameListElement extends React.Component {
 														fillRule="nonzero"
 													></path>
 												</SvgIcon>
-											) : this.state.member
-													.NewestPhaseState
-													.ReadyToResolve ? (
+											) : this.state.member.NewestPhaseState.ReadyToResolve ? (
 												<SvgIcon>
 													<path
 														d="M9,0 C10.3,0 11.4,0.84 11.82,2 L11.82,2 L16,2 C17.1045695,2 18,2.8954305 18,4 L18,4 L18,18 C18,19.1045695 17.1045695,20 16,20 L16,20 L2,20 C0.8954305,20 0,19.1045695 0,18 L0,18 L0,4 C0,2.8954305 0.8954305,2 2,2 L2,2 L6.18,2 C6.6,0.84 7.7,0 9,0 Z M13.4347826,7 L7.70608696,12.7391304 L4.56521739,9.60869565 L3,11.173913 L7.70608696,15.8695652 L15,8.56521739 L13.4347826,7 Z M9,2 C8.44771525,2 8,2.44771525 8,3 C8,3.55228475 8.44771525,4 9,4 C9.55228475,4 10,3.55228475 10,3 C10,2.44771525 9.55228475,2 9,2 Z"
@@ -860,17 +916,13 @@ class GameListElement extends React.Component {
 										>
 											{this.state.game.Properties.Finished
 												? helpers.minutesToDuration(
-														-this.state.game
-															.Properties
-															.FinishedAgo /
+														-this.state.game.Properties.FinishedAgo /
 															1000000000 /
 															60,
 														true
 												  )
 												: helpers.minutesToDuration(
-														this.state.game
-															.Properties
-															.NewestPhaseMeta[0]
+														this.state.game.Properties.NewestPhaseMeta[0]
 															.NextDeadlineIn /
 															1000000000 /
 															60,
@@ -879,10 +931,7 @@ class GameListElement extends React.Component {
 										</Typography>
 									</div>
 								</div>
-								<div
-									key={itemKey++}
-									style={secondRowSummaryClass}
-								>
+								<div key={itemKey++} style={secondRowSummaryClass}>
 									<Typography
 										textroverflow="ellipsis"
 										noWrap={true}
@@ -892,47 +941,27 @@ class GameListElement extends React.Component {
 										style={secondRowSummaryColorClass}
 									>
 										{this.state.game.Properties.Variant}{" "}
-										{helpers.phaseLengthDisplay(
-											this.state.game.Properties
-										)}
+										{helpers.phaseLengthDisplay(this.state.game.Properties)}
 									</Typography>
 									<div style={summaryIconsAndPhaseClass}>
-										<div style={summaryIconsClass}>
-											{this.getIcons()}
-										</div>
+										<div style={summaryIconsClass}>{this.getIcons()}</div>
 										<Typography
 											variant="caption"
 											style={secondRowSummaryColorClass}
 										>
-											{
-												this.state.game.Properties
-													.NewestPhaseMeta[0].Season
-											}{" "}
-											{
-												this.state.game.Properties
-													.NewestPhaseMeta[0].Year
-											}
-											,{" "}
-											{
-												this.state.game.Properties
-													.NewestPhaseMeta[0].Type
-											}
+											{this.state.game.Properties.NewestPhaseMeta[0].Season}{" "}
+											{this.state.game.Properties.NewestPhaseMeta[0].Year},{" "}
+											{this.state.game.Properties.NewestPhaseMeta[0].Type}
 										</Typography>
 									</div>
 								</div>
-								{!this.state.member ||
-								this.state.game.Properties.Mustered ? (
+								{!this.state.member || this.state.game.Properties.Mustered ? (
 									""
-								) : (
-										this.state.game.Properties.Members || []
-								  ).find((m) => {
+								) : (this.state.game.Properties.Members || []).find((m) => {
 										return m.User.Id === Globals.user.Id;
 								  }).NewestPhaseState.ReadyToResolve ? (
 									<Typography
-										className={
-											this.props.classes
-												.sixteenBySixteenClass
-										}
+										className={this.props.classes.sixteenBySixteenClass}
 									>
 										Confirmed ready <ConfirmedReadyIcon />
 									</Typography>
@@ -951,25 +980,19 @@ class GameListElement extends React.Component {
 												.safeFetch(
 													helpers.createRequest(
 														"/Game/" +
-															this.state.game
-																.Properties.ID +
+															this.state.game.Properties.ID +
 															"/Phase/" +
-															this.state.game
-																.Properties
-																.NewestPhaseMeta[0]
+															this.state.game.Properties.NewestPhaseMeta[0]
 																.PhaseOrdinal +
 															"/PhaseState",
 														{
 															headers: {
-																"Content-Type":
-																	"application/json",
+																"Content-Type": "application/json",
 															},
 															method: "PUT",
-															body: JSON.stringify(
-																{
-																	ReadyToResolve: true,
-																}
-															),
+															body: JSON.stringify({
+																ReadyToResolve: true,
+															}),
 														}
 													)
 												)
@@ -1012,19 +1035,9 @@ class GameListElement extends React.Component {
 										}}
 									>
 										<NumMembersIcon />
-										<Typography
-											variant="body2"
-											style={{ paddingLeft: "2px" }}
-										>
-											{
-												this.state.game.Properties
-													.NMembers
-											}
-											/
-											{
-												this.variant.Properties.Nations
-													.length
-											}{" "}
+										<Typography variant="body2" style={{ paddingLeft: "2px" }}>
+											{this.state.game.Properties.NMembers}/
+											{this.variant.Properties.Nations.length}{" "}
 										</Typography>
 									</div>
 								</div>
@@ -1045,9 +1058,7 @@ class GameListElement extends React.Component {
 										}}
 									>
 										{this.state.game.Properties.Variant}{" "}
-										{helpers.phaseLengthDisplay(
-											this.state.game.Properties
-										)}
+										{helpers.phaseLengthDisplay(this.state.game.Properties)}
 									</Typography>
 									<div> {this.getIcons()}</div>
 								</div>
@@ -1162,11 +1173,7 @@ class GameListElement extends React.Component {
 									>
 										{buttonDiv}
 										{this.state.dead ? (
-											<Typography
-												style={{ color: "red" }}
-											>
-												Deleted
-											</Typography>
+											<Typography style={{ color: "red" }}>Deleted</Typography>
 										) : (
 											""
 										)}
