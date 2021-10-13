3d4924137ba0702cdf25d9832a2391271a338288
= <=>
2021-10-13 11:44:13 +0100

    wipwipwip

--- web-app/src/components/DipMap.tsx
+++ web-app/src/components/DipMap.tsx
@@ -1,5 +1,10 @@
 import { makeStyles } from "@material-ui/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { hash } from "../utils/general";
+import * as helpers from "../helpers";
+import { Phase, Variant } from "../store/types";
+import { variant } from "../store/testData";
 
 const useStyles = makeStyles((theme) => ({
   viewport: {
@@ -21,31 +26,97 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const DipMap = () => {
+  const [svgLoaded, setSvgLoaded] = useState(true);
+  const [lastRenderedPhaseHash, setLastRenderedPhaseHash] = useState<
+    number | null
+  >(null);
 
-  const [svgLoaded, setSvgLoaded] = useState(false);
+  // TODO typing
+  const [phaseSpecialStrokes, setPhaseSpecialStrokes] = useState({});
 
   // Props
   const currentNation = "";
+  const phase: Phase = {
+    PhaseOrdinal: 1,
+    Season: "",
+    Year: 1,
+    Type: "",
+    Resolved: false,
+    CreatedAt: "",
+    CreatedAgo: 1,
+    ResolvedAt: "",
+    ResolvedAgo: 1,
+    DeadlineAt: "",
+    NextDeadlineIn: 1,
+    UnitsJSON: "",
+    SCsJSON: "",
+    GameID: "",
+    Units: [],
+    SCs: [],
+    Dislodgeds: null,
+    Dislodgers: null,
+    ForceDisbands: null,
+    Bounces: null,
+    Resolutions: null,
+    Host: "",
+    SoloSCCount: 1,
+    PreliminaryScores: [],
+  };
+
+  const variant: Variant = {
+    ...variant,
+  };
 
   const classes = useStyles();
 
   // TODO
-  const showInfoProvinceInfo = (province: string) => {
-
-  }
+  const showInfoProvinceInfo = (province: string) => {};
 
   // TODO
-  const onClickProvince = (province: string) => {
-
-  }
+  const onClickProvince = (province: string) => {};
 
   const updateMap = () => {
-      if (!svgLoaded) return; // TODO explainer comment
+    if (!svgLoaded) return; // TODO explainer comment
 
-      // TODO set map subtitle
-  }
+    const phaseHash = hash(JSON.stringify(phase));
+    console.log(phaseHash);
+    const nodes = variant.Start?.Graph.Nodes;
 
+    // TODO use compare?
+    if (phaseHash !== lastRenderedPhaseHash) {
+      setLastRenderedPhaseHash(phaseHash);
+      let tempPhaseSpecialStrokes: { [key: string]: string } = {};
+      let SCs: { [key: string]: string } = {};
 
+      // if (phase.SCs) {
+      //   SCs = phase.SCs;
+      // } else {
+      phase.SCs.forEach((supplyCenter) => {
+        SCs[supplyCenter.Province] = supplyCenter.Owner;
+      });
+
+      for (let prov in nodes) {
+        const node = nodes[prov]; // TODO what's this about?
+        if (node.SC && SCs[prov]) {
+          const color = helpers.natCol(SCs[prov], variant);
+          if (helpers.brightnessByColor(color) || 0 < 0.5) {
+            // TODO fix
+            tempPhaseSpecialStrokes[prov] = "#ffffff";
+            // this.map.colorSC(prov, "ffffff"); TODO
+          }
+        } else {
+          // this.map.hideProvince(prov)
+        }
+      }
+      // }
+    }
+
+    // TODO set map subtitle
+  };
+
+  useEffect(() => {
+    updateMap();
+  });
 
   return (
     <>
