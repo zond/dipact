941c40d29d8ff1fc951f97832ed7cd3c0eb93038
John McDowell <johnmcdowell@microsoft.com>
2023-01-12 17:56:47 +0000

    Wip fix debug build

--- packages/native-app/babel.config.js
+++ packages/native-app/babel.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
   plugins: [
+    "react-native-reanimated/plugin",
     [
       "module-resolver",
       {
         root: ["./src"],
         alias: {
-          "react": "./node_modules/react",
+          react: "./node_modules/react",
           "react-redux": "./node_modules/react-redux",
           "react-ga": "./node_modules/react-ga",
         },
--- packages/native-app/src/utils.ts
+++ packages/native-app/src/utils.ts
@@ -1,5 +1,5 @@
 export const getLoginUrl = (): string => {
-  const redirectUrl = "https://www.diplicitynative.com/auth";
+  const redirectUrl = "https://www.diplicity.com";
   const tokenDuration = 60 * 60 * 24;
   return `https://diplicity-engine.appspot.com/Auth/Login?redirect-to=${encodeURI(
     redirectUrl
