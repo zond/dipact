4a738e024d7a4783b390950daa208e6d360715c0
John McDowell <johnmcdowell@microsoft.com>
2023-01-20 15:51:03 +0000

    Fix issue with Login on Android

--- packages/native-app/src/utils.ts
+++ packages/native-app/src/utils.ts
@@ -1,5 +1,5 @@
 export const getLoginUrl = (): string => {
-  const redirectUrl = "https://www.diplicitynative.com/auth";
+  const redirectUrl = "diplicity-native://auth";
   const tokenDuration = 60 * 60 * 24;
   return `https://diplicity-engine.appspot.com/Auth/Login?redirect-to=${encodeURI(
     redirectUrl
