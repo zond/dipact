104e4510039127219323c2f9194c176cbfd84e69
John McDowell <johnmcdowell@microsoft.com>
2023-05-23 16:36:09 +0100

    Add webpack config for startup

--- packages/web-app/src/pages/Map.tsx
+++ packages/web-app/src/pages/Map.tsx
@@ -15,26 +15,26 @@ interface MapUrlParams {
 const Map = () => {
   const { gameId } = useParams<MapUrlParams>();
   const { setParam } = useSearchParams();
-  const { data, isLoading } = useMap(gameId);
+  // const { data, isLoading } = useMap(gameId);
 
-  // if (isError && error) return <ErrorMessage error={error as DiplicityError} />;
-  if (isLoading) return <Loading />;
+  // // if (isError && error) return <ErrorMessage error={error as DiplicityError} />;
+  // if (isLoading) return <Loading />;
 
-  return (
-    <>
-      {data && <Svg src={data} />}
-      <CreateOrderMenu />
-      <Fab
-        color="primary"
-        aria-label="add"
-        // TODO move fab position to constants
-        style={{ position: "fixed", bottom: "20px", right: "20px" }}
-        onClick={() => setParam(searchKey, "1")}
-      >
-        <CreateOrderIcon />
-      </Fab>
-    </>
-  );
+  // return (
+  //   <>
+  //     {data && <Svg src={data} />}
+  //     <CreateOrderMenu />
+  //     <Fab
+  //       color="primary"
+  //       aria-label="add"
+  //       // TODO move fab position to constants
+  //       style={{ position: "fixed", bottom: "20px", right: "20px" }}
+  //       onClick={() => setParam(searchKey, "1")}
+  //     >
+  //       <CreateOrderIcon />
+  //     </Fab>
+  //   </>
+  // );
 };
 
 export default Map;
---
+++ packages/web-app/webpack.config.js
@@ -0,0 +1,54 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (env, args) => {
+  return {
+    entry: "./src/index.js",
+    output: {
+      publicPath: "/",
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+    ],
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.svg$/,
+          use: ["@svgr/webpack"],
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          use: "file-loader",
+        },
+      ],
+    },
+    devServer: {
+      hot: true,
+      // static: path.join(__dirname, "public"),
+      // port: 3000,
+      // host: "localhost",
+      // historyApiFallback: true,
+      // open: true,
+    },
+  };
+};
